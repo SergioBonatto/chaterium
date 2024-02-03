bd190419117cb248f96c090d9cfeb6ae2cfc9cdf
Sergio Bonatto <52257001+SergioBonatto@users.noreply.github.com>
2024-02-03 01:22:00 -0300

    add: app (done)

--- src/App.rs
+++ src/App.rs
@@ -79,7 +79,86 @@ impl App {
             true
     }
     
+        fn is_chain_valid(&self, chain: &[Block]) -> bool {
+            for i in 0..chain.len() {
+                if i == o {
+                    continue;
+                } 
+                let first = chain.get(i - 1).expect("has to exist");
+                let second = chain.get(i).expect("has to exist");
+                if !self.is_block_valid(second, first) {
+                    return false;
+                }
+            }
+            true
+        }
 
+       // we always choose the longest chain
+        fn choose_chain(&mut self, local: Vec, remote: Vec) -> Vec {
+            let is_local_valid = self.is_chain_valid(&local);
+            let is_remote_valid = self.is_chain_valid(&remote);
+
+            if is_local_valid && is_remote_valid {
+                if local.len() >= remote.len() {
+                    local
+                } else {
+                    remote
+                }
+            } else if is_remote_valid && !is_local_valid {
+                remote
+            } else if !is_remote_valid && is_local_valid {
+                local
+            } else {
+                panic!("local and remote chains are both invalid");
+            }
+        }
+}
+impl Block {
+    pub fn new(id: u64, previous_hash: String, data: String) -> Self {
+        let now = Utc::now();
+        let (nonce, hash) = mine_block(id, now.timestamp(), &previous_hash, &data);
+        Self {
+            id,
+            hash,
+            timestamp: now.timestamp(),
+            previous_hash,
+            data,
+            nonce,
+        }
+    }
+
+    fn mine_block(id: i64, timestamp: i64, previous_hash: &str, data: &str) -> (u64, String) {
+        info!("mining block...");
+        let mut nonce = 0;
+        loop {
+            if nonce % 100000 == 0 {
+                info!("nonce: {}", nonde);
+            } 
+            let hash = calculate_hash(id, timestamp, previous_hash, data, nonce);
+            let binary_hash = hash_to_binary_representation(&hash);
+            if binary_hash.starts_with(DIFFICULTY_PREFIX) {
+                info!(
+                    "mined! nonce: {}, hash: {}, binary_hash: {}",
+                    nonce, hash, binary_hash
+                );
+                return (nonce, hex::encode(hash));
+            }
+            nonce += 1;
+        }
+    } 
+
+    fn calculate_hash(id: u64, timestamp: i64, previous_hash: &str, data: &str, nonce: u64) -> Vec<u8> {
+        let data = serde_json::json!({
+            "id": id,
+            "previous_hash": previous_hash,
+            "data": data,
+            "timestamp": timestamp,
+            "nonce": nonce,
+        });
+        let mut hasher = Sha256::new();
+        hasher.update(data.to_string().as_bytes());
+        hasher.finalize().as_slice().to_owned()
+    }
 
 }
 
